36abdf79993d74f41d577a8750029819ef2a7cdd
Arshad <aliarshad.memon749@gmail.com>
2020-11-15 21:22:30 +0500

    just created the spring starter project with required dependencies

---
+++ src/main/java/com/api/scm/main/SmartContactManagerApplication.java
@@ -0,0 +1,13 @@
+package com.api.scm.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartContactManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartContactManagerApplication.class, args);
+	}
+
+}
