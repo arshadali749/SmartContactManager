334b479d542bbf8703a0b99f97479c6b84fbed35
Arshad <aliarshad.memon749@gmail.com>
2020-11-22 12:20:35 +0500

    Designed home page and just created login,signup and about pages.

--- src/main/java/com/api/scm/main/controllers/HomeController.java
+++ src/main/java/com/api/scm/main/controllers/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
 
-	@RequestMapping("/home")
+	@RequestMapping("/")
 	public String getHomePage(Model model) {
 		model.addAttribute("title","index.html");
 		return "index";
@@ -18,6 +18,18 @@ public class HomeController {
 	public String getAboutPage(Model model) {
 		model.addAttribute("title","about.html");
 		return "about";
+	}
+	
+	@RequestMapping("/signup")
+	public String getSignUpPage(Model model) {
+		model.addAttribute("title","signup.html");
+		return "signup";
+	}
+	
+	@RequestMapping("/login")
+	public String getLoginPage(Model model) {
+		model.addAttribute("title","login.html");
+		return "login";
 	}
 
 }
