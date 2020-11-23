441c2a2f8fc950dda28a2368d783be0432334122
Arshad <aliarshad.memon749@gmail.com>
2020-11-23 17:40:50 +0500

    designed the sidebar menu , working fine

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,15 @@
+function  toggleSideBar(){
+    if($(".sidebar").is(":visible")){
+  $(".sidebar").css("display","none");
+  $(".content").css("margin-left","0%")
+    }
+    else{
+        $(".sidebar").css("display","block");
+        $(".content").css("margin-left","20%")
+         
+    }
+};
+
+function test(){
+    alert();
+}
